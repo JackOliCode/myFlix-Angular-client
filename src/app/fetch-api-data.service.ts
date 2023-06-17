3fed24edfe26506da4c6dc574167e1f7ce6a7f7f
JackOliCode <jack.v.oliver@gmail.com>
2023-06-17 17:44:34 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://jackoc-myflix.onrender.com/';
 @Injectable({ // A decorator is a function that augments a piece of code—usually another function or a class.//
   providedIn: 'root'
 })
